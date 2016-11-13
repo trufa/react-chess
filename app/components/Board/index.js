52b7c77f68f85734967ccd63db4612ba535a68d7
Trufa <trufavarela@gmail.com>
2016-11-13 20:09:22 -0300

    use templates instead of concat

--- app/components/Board/index.js
+++ app/components/Board/index.js
@@ -9,7 +9,7 @@ import Square from 'components/Square';
 
 function Board(props) {
   const squareComponents = props.boardArray.map((item, index) => {
-    const sq = [<Square id={item} key={index} />];
+    const sq = [<Square id={`${index} - ${item}`} key={index} />];
     if ((index + 1) % 10 === 0) {
       sq.push(<br />);
     }
